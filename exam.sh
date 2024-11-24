a4f374867602b3bf6f476f92e9bd4e3634f1fe8d
fbarulli <fbarulli@gmail.com>
2024-11-24 15:44:59 +0100

    sprint 4 stuff

--- exam.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-while true; do
-    curl "http://0.0.0.0:5000/" >> sales.txt
-    sleep 60
-done 
--- hello.py
+++
@@ -1,6 +0,0 @@
-def main():
-    print("Hello from mle!")
-
-
-if __name__ == "__main__":
-    main()
